49ab58f56944d6f3571ec9f0979babf3a4f836b8
Shady Boukhary <sb199898.sb@gmail.com>
2019-06-24 22:24:25 -0400

    Created state class

---
+++ include/state.hpp
@@ -0,0 +1,26 @@
+#ifndef __STATE_HPP__
+#define __STATE_HPP__
+
+#define DEFAULT_ENERGY 1800
+#include "location.hpp"
+
+class State
+{
+public:
+    State();
+    State(Location location, uint64_t energy, bool isAlive);
+    State(const State& other) = default;
+    State(State&&) = default;
+    Location getLocation() const;
+    uint64_t getEnergy() const;
+    bool isAlive() const;
+
+    bool operator==(const State &);
+    State& operator=(State &) & = default;
+private:
+    Location location;
+    uint64_t energy;
+    bool alive;
+};
+
+#endif
